cf6ef6987c0569b8921218329ec56e43fd7d3360
Omar Gonzalez <ogonzalez2212@gmail.com>
2016-08-16 16:31:03 -0500

    Commit #1 - CrearTorneo agregado

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,2 +1,16 @@
 class TournamentsController < ApplicationController
+
+  def create
+    if(Tournament.find_by(name: params[:name]) == nil)
+      t = Tournament.new
+      t.name = params[:name]
+      t.minPlayers = params[:min]
+      t.status = false
+      t.save
+      redirect_to '/', notice: "Torneo Creado"
+    else
+      redirect_to '/', alert: "Error, Torneo ya existente"
+    end
+  end
+
 end
