71da890f6bbc8f44ca75daa39ab7be124da53dae
OmarGM2294 <ogonzalez2212@gmail.com>
2016-08-20 21:40:05 -0500

    Commit #8 - Bugs

--- db/migrate/20160819153806_add_column_winner_to_tournament.rb
+++ db/migrate/20160819153806_add_column_winner_to_tournament.rb
@@ -1,5 +1,5 @@
 class AddColumnWinnerToTournament < ActiveRecord::Migration
   def change
-    add_column :tournament, :winner_id , :integer, index: true, :foreign_key => 'Player' 
+    add_column :tournaments, :winner_id , :integer, index: true, :foreign_key => 'Player' 
   end
 end
